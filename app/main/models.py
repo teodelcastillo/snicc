27f1bb8679520a5e59a0c8bf9e6fc898e3fa675d
Julian Eduardo Orrillo <jorrillo@MEDIOAMBIENTE.GOV.AR>
2025-07-18 18:17:23 -0300

    se hicieron cambios en titulo y link en models.py

--- app/main/models.py
+++ app/main/models.py
@@ -305,7 +305,7 @@ class Book(models.Model, VersionedNoDate):
     year = models.IntegerField(default=2025)
     category = models.CharField(choices=CategoryExtended, max_length=50, default=CategoryExtended.ampyd)
     date = models.DateTimeField(auto_now=True)
-    url = models.URLField(null=True, blank=True,,max_length=300)
+    url = models.URLField(null=True, blank=True,max_length=300)
     # title = models.CharField(max_length=100)
     # description = models.TextField(null=True)
     authors = models.ManyToManyField(Author, blank=True)
@@ -345,7 +345,7 @@ class InternalLink(models.Model, VersionedNoDate):
     # not an UrlField
     view = models.CharField(max_length=50, null=True, blank=True, help_text='Exactly one of this or "url" must be set.')
     viewargs = models.CharField(max_length=50, null=True, blank=True)
-    url = models.URLField(null=True, blank=True, help_text='Exactly one of this or "view" must be set.',,max_length=300)
+    url = models.URLField(null=True, blank=True, help_text='Exactly one of this or "view" must be set.',max_length=300)
 
     def __str__(self):
         if self.view:
